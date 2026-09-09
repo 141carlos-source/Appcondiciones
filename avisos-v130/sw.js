6de13db10818f1499b1e7546b6b36584cd921991
141carlos-source <141carlos@gmail.com>
2026-09-09 23:48:35 +0200

    V1.30: renovar caché de agrupación por dirección

--- avisos-v130/sw.js
+++ avisos-v130/sw.js
@@ -1,4 +1,4 @@
-const CACHE='APP-AVISOS-V130-SHELL-1';
+const CACHE='APP-AVISOS-V130-SHELL-2';
 const SHELL=['./','./index.html','./manifest.webmanifest','./install.js','./expediente.js','../avisos-v129/icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE&&k.startsWith('APP-AVISOS-V130-')).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
