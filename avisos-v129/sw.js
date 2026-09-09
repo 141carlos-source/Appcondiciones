572f963f410dcb64d5ae226a27c153be9e9eccc5
141carlos-source <141carlos@gmail.com>
2026-09-09 23:40:14 +0200

    V1.29: renovar caché de ficha de cliente

--- avisos-v129/sw.js
+++ avisos-v129/sw.js
@@ -1,5 +1,5 @@
-const CACHE='APP-AVISOS-V129-SHELL-8';
-const SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg','./install.js','./home.js','./suministro.js','./condiciones-flow.js','./layout.js'];
+const CACHE='APP-AVISOS-V129-SHELL-9';
+const SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg','./install.js','./home.js','./suministro.js','./condiciones-flow.js','./layout.js','./client-summary.js'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE&&k.startsWith('APP-AVISOS-V129-')).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(r=>{if(r&&r.ok&&new URL(event.request.url).origin===location.origin){const copy=r.clone();caches.open(CACHE).then(c=>c.put(event.request,copy)).catch(()=>{})}return r}).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html'))))});
