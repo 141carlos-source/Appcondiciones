9d7852c0f094cd50603c9052c75522d4394b934d
141carlos-source <141carlos@gmail.com>
2026-09-10 07:19:27 +0200

    V1.33: renovar caché tras selección múltiple de fotos

--- avisos-v133/sw.js
+++ avisos-v133/sw.js
@@ -1,4 +1,4 @@
-const CACHE='APP-AVISOS-V133-SHELL-4';
+const CACHE='APP-AVISOS-V133-SHELL-5';
 const SHELL=['./','./index.html','./core-template.html','./app133.js','./patch133.js','./plan133.js','./photos133.js','./photos133fix.js','./cnmc133.js','./manifest.webmanifest','./icon.svg','./install.js'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE&&k.startsWith('APP-AVISOS-V133-')).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
