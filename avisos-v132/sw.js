e8aeaa1a533d9acca5aba776d69766d35f0b63b0
141carlos-source <141carlos@gmail.com>
2026-09-10 05:59:54 +0200

    V1.32: añadir service worker autónomo

---
+++ avisos-v132/sw.js
@@ -0,0 +1,5 @@
+const CACHE='APP-AVISOS-V132-SHELL-1';
+const SHELL=['./','./index.html','./core-template.html','./app132.js','./patch132.js','./manifest.webmanifest','./icon.svg','./install.js'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE&&k.startsWith('APP-AVISOS-V132-')).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;const u=new URL(event.request.url);if(u.origin!==location.origin)return;event.respondWith(fetch(event.request).then(r=>{if(r&&r.ok){const c=r.clone();caches.open(CACHE).then(x=>x.put(event.request,c)).catch(()=>{})}return r}).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html'))))});
