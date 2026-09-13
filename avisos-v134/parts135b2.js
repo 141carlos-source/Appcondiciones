27594f2c3eca5a36a375de76e1bdcb005c556240
141carlos-source <141carlos@gmail.com>
2026-09-13 21:20:38 +0200

    Probar campos Beta 2

--- avisos-v134/parts135b2.js
+++ avisos-v134/parts135b2.js
@@ -1,1 +1,1 @@
-(function(){'use strict';window.Soltec135Beta2Ready='V1.35-BETA2-20260913';function testFileReader(){return typeof FileReader!=='undefined'}})();
+(function(){'use strict';window.Soltec135Beta2Ready='V1.35-BETA2-20260913';function testFileReader(){return typeof FileReader!=='undefined'}const campo='firma cliente nif';})();
