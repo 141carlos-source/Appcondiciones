da78455259002e3a1f6b5c26c35d5b8a99e5856a
141carlos-source <141carlos@gmail.com>
2026-09-13 21:17:33 +0200

    Probar base técnica Beta 2

--- avisos-v134/parts135b2.js
+++ avisos-v134/parts135b2.js
@@ -1,1 +1,1 @@
-(function(){'use strict';window.Soltec135Beta2Ready='V1.35-BETA2-20260913';})();
+(function(){'use strict';window.Soltec135Beta2Ready='V1.35-BETA2-20260913';function testFileReader(){return typeof FileReader!=='undefined'}})();
