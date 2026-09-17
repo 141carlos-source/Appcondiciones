ec85f3024f19087fd350648eeaaf7e9901adeb76
141carlos-source <141carlos@gmail.com>
2026-09-17 06:53:35 +0200

    Detener recarga continua de la pantalla

--- avisos-v134/appupdate134.js
+++ avisos-v134/appupdate134.js
@@ -1,5 +1,5 @@
 (function(){'use strict';
-const BUILD='V135-MODULOS-9-CNMC-PLANO-CP';
+const BUILD='V135-MODULOS-10-TECNICA-CNMC';
 let checking=false,lastCheck=0;
 function loadSafe(){try{if(!document.getElementById('s135SafeExit')){let s=document.createElement('script');s.id='s135SafeExit';s.src='./safeexit135.js?build='+encodeURIComponent(BUILD)+'&t='+Date.now();document.head.appendChild(s)}else if(window.Soltec135SafeExit&&window.Soltec135SafeExit.init){window.Soltec135SafeExit.init()}}catch(e){}}
 function loadRecovery(){try{if(!document.getElementById('s134Recovery')){let s=document.createElement('script');s.id='s134Recovery';s.src='./sync134recovery.js?build='+encodeURIComponent(BUILD)+'&t='+Date.now();s.onload=loadRevFix;document.head.appendChild(s)}else{if(window.Soltec134Recovery&&window.Soltec134Recovery.init)window.Soltec134Recovery.init();loadRevFix()}}catch(e){}}
