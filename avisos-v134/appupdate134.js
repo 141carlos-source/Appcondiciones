5eda7c5b20cd2eb870d941bd7095cde7d85d3c80
141carlos-source <141carlos@gmail.com>
2026-09-18 21:01:46 +0200

    Build V135-AGENDA-INAPP1: Agenda dentro de SOLTEC

--- avisos-v134/appupdate134.js
+++ avisos-v134/appupdate134.js
@@ -1,5 +1,5 @@
 (function(){'use strict';
-const BUILD='V135-PARTES-FOTOS3';
+const BUILD='V135-AGENDA-INAPP1';
 let checking=false,lastCheck=0;
 function loadSafe(){try{if(!document.getElementById('s135SafeExit')){let s=document.createElement('script');s.id='s135SafeExit';s.src='./safeexit135.js?build='+encodeURIComponent(BUILD)+'&t='+Date.now();document.head.appendChild(s)}else if(window.Soltec135SafeExit&&window.Soltec135SafeExit.init){window.Soltec135SafeExit.init()}}catch(e){}}
 function loadSafeAutoHide(){try{if(!document.getElementById('s135SafeExitAutoHide')){let s=document.createElement('script');s.id='s135SafeExitAutoHide';s.src='./safeexit135autohide.js?build='+encodeURIComponent(BUILD)+'&t='+Date.now();document.head.appendChild(s)}else if(window.Soltec135SafeExitAutoHide&&window.Soltec135SafeExitAutoHide.init){window.Soltec135SafeExitAutoHide.init()}}catch(e){}}
