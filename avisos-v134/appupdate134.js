3d7f3edade3532ab4ade4616bbc2d5d6f4a45c81
141carlos-source <141carlos@gmail.com>
2026-09-22 22:50:56 +0200

    Publish direct PDF attach in Expedientes

--- avisos-v134/appupdate134.js
+++ avisos-v134/appupdate134.js
@@ -1,5 +1,5 @@
 (function(){'use strict';
-const BUILD='V135-COND-PDF-EXP1';
+const BUILD='V135-COND-PDF-EXP2';
 let checking=false,lastCheck=0;
 function loadSafe(){try{if(!document.getElementById('s135SafeExit')){let s=document.createElement('script');s.id='s135SafeExit';s.src='./safeexit135.js?build='+encodeURIComponent(BUILD)+'&t='+Date.now();document.head.appendChild(s)}else if(window.Soltec135SafeExit&&window.Soltec135SafeExit.init){window.Soltec135SafeExit.init()}}catch(e){}}
 function loadSafeAutoHide(){try{if(!document.getElementById('s135SafeExitAutoHide')){let s=document.createElement('script');s.id='s135SafeExitAutoHide';s.src='./safeexit135autohide.js?build='+encodeURIComponent(BUILD)+'&t='+Date.now();document.head.appendChild(s)}else if(window.Soltec135SafeExitAutoHide&&window.Soltec135SafeExitAutoHide.init){window.Soltec135SafeExitAutoHide.init()}}catch(e){}}
